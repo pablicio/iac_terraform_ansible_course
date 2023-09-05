81830dc0d8587c637c7e9fb215b0bada6199f646
pablicio <pabliciotjg@gmail.com>
2023-09-05 16:12:22 -0300

    Add swagger suport

--- c4_front/c4-visualization/src/components/SwaggerRender.vue
+++ c4_front/c4-visualization/src/components/SwaggerRender.vue
@@ -5,8 +5,8 @@
 </template>
 
 <script>
-import SwaggerUIBundle  from '@/assets/swagger/swagger-ui-bundle.js'
-import  SwaggerUIStandalonePreset  from '@/assets/swagger/swagger-ui-standalone-preset.js'
+import SwaggerUIBundle from '@/assets/swagger/swagger-ui-bundle.js'
+import SwaggerUIStandalonePreset from '@/assets/swagger/swagger-ui-standalone-preset.js'
 import '@/assets/swagger/swagger-ui.css'
 
 export default {
